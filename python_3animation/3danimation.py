885bebdee081afdd404aaee0d9f1d845547bbc8c
tomoki <nishimuratk1105@gmail.com>
2024-02-03 17:23:49 +0900

    高階関数を利用するように改善

--- python_3animation/3danimation.py
+++ python_3animation/3danimation.py
@@ -23,22 +23,27 @@ class Animation:
     """
     #背景の初期値(黒)
     BG = (0,0,0)
-    def __init__(self, width, height, background=BG):
-        self.size = (width, height)
-        self.background = background
-    
-    def get_surf_list(self, n):
+
+    @staticmethod
+    def rotation_square(n):
         surf_list = []
         theta_list = [(a+n/60) * np.pi for a in [0,-1/2,-1,-3/2]]
-        surface = [[500+300*np.cos(theta), 500+300*np.sin(theta)] for theta in theta_list]
+        surface = [[300*np.cos(theta), 300*np.sin(theta)] for theta in theta_list]
         surf_list.append(surface)
         return surf_list
+
+    def __init__(self, size, background=BG, get_surf_func=rotation_square):
+        self.size = size
+        self.background = background
+        self.get_surf_func = get_surf_func
     
     def _mask(self, surface):
         """
         surfaceは凸平面の各店を時計回りに指定
         """
         x,y = np.indices(self.size)
+        x -= self.size[0]//2
+        y -= self.size[1]//2
         X = np.stack((x,y,np.ones(self.size)), axis=2)
         #ずっと使いまわすので，どこかに保管したほうが効率がよさそう?
 
@@ -52,7 +57,6 @@ class Animation:
             c = p2[0] * p1[1] - p1[0] * p2[1]
             k = np.array([a,b,c])
             k_list[:,i] = k
-
         mask = (X@k_list>0).all(2)
         return mask
         
@@ -70,7 +74,7 @@ class Animation:
     def export(self, path, N, duration=100):
         images = np.zeros((N,)+self.size+(3,), dtype=np.uint8)
         for n in range(N):
-            surf_list = self.get_surf_list(n)
+            surf_list = self.get_surf_func(n)
             im = self.draw_image(surf_list)
             images[n] = im
             if(n%10==0):
@@ -78,6 +82,16 @@ class Animation:
         PIL_images = [Image.fromarray(im) for im in images]
         PIL_images[0].save(path, save_all=True, append_images=PIL_images[1:], optimize=False, duration=duration, loop=0)
 if __name__ == "__main__":
-    anime = Animation(1000,1000)
+    def make_rotation_polygon(n):
+        def rotate_polygon(time):
+            surf_list = []
+            A = [-2/n*a for a in range(n)]
+            theta_list = [(a+time/60) * np.pi for a in A]
+            surface = [[300*np.cos(theta), 300*np.sin(theta)] for theta in theta_list]
+            surf_list.append(surface)
+            return surf_list
+        return rotate_polygon
+
+    anime = Animation((1000,1000), get_surf_func=make_rotation_polygon(6))
     path = "./output/anime1.gif"
     anime.export(path, 300, 1000/60)
